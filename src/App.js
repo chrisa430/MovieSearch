15bf2dad7dabb6b2eb954f4076a5edf65a317f55
Chris Argyros <cargyros430@gmail.com>
2022-09-11 18:32:08 -0500

    movie card loop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useEffect} from "react";
+import {useState, useEffect} from "react";
 import './App.css';
 import SearchIcon from './search.svg';
 import MovieCard from "./MovieCard";
@@ -14,12 +14,13 @@ const movie1 = {
 }
 
 const App = () =>{
+    const [movies, setMovies] = useState([]);
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
-        console.log(data.Search);
+        setMovies(data.Search);
     }
 
     useEffect(() => {
@@ -42,9 +43,19 @@ const App = () =>{
        onClick={() => {}}
        />
        </div>
-       <div className="container">
-           <MovieCard movie1={movie1}/>
-       </div>
+
+    {movies?.length > 0 
+      ? (
+        <div className="container">
+            {movies.map((movie) => (
+                <MovieCard movie={movie}/>
+            ))}
+        </div>
+        ) : (
+            <div className="empty">
+                <h2>No movies found</h2>
+            </div>
+        )}
        </div>
     );
 }
